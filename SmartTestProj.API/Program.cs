d7bfacf2ddcde0358624cd55287430d75f9c53d4
Maksym Mediuh <maksim2003mmm15@gmail.com>
2024-12-16 23:47:36 +0200

    Add transient fieds for repos and services

--- SmartTestProj.API/Program.cs
+++ SmartTestProj.API/Program.cs
@@ -1,9 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using SmartTestProj.BLL.Services.Interfaces;
+using SmartTestProj.BLL.Services.Realizations;
+using SmartTestProj.DAL.Context;
+using SmartTestProj.DAL.Repository.Interface;
+using SmartTestProj.DAL.Repository.Realization;
 
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlServer<AppDbContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+builder.Services.AddScoped<IEquipmentPlacementContractRepository, EquipmentPlacementContractRepository>();
+builder.Services.AddScoped<IProductionFacilityRepository, ProductionFacilityRepository>();
+builder.Services.AddScoped<IProcessEquipmentTypeRepository, ProcessEquipmentTypeRepository>();
+
+
+builder.Services.AddTransient<IEquipmentPlacementContractService, EquipmentPlacementContractService>();
+builder.Services.AddTransient<IProductionFacilityService, ProductionFacilityService>();
+builder.Services.AddTransient<IProcessEquipmentTypeService, ProcessEquipmentTypeService>();
 
 var app = builder.Build();
 
